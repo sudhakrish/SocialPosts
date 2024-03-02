82bfe1fbb44352b2406facb068f3668f7fe95c52
Sudhakar <pingtosudhakar@gmail.com>
2024-03-02 15:43:45 +0800

    Initial Commit

---
+++ SocialPosts/SocialPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialPostsApp.swift
+//  SocialPosts
+//
+//  Created by Sudhakar Krishnan on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
